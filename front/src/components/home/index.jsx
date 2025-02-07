0bd0e2f4d41c22d75412b5b3229cd5339f4b2d31
Catifundo <lindomarbatistao@gmail.com>
2025-02-07 16:00:09 -0300

    07-02-25 16:00

--- front/src/components/home/index.jsx
+++ front/src/components/home/index.jsx
@@ -1,14 +1,18 @@
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { FaEdit, FaTrash, FaPlus, FaSearch } from 'react-icons/fa'
+import './styles.css'
 
-
-export default function Home(){
+export default function Home() {
+    const [dados, setDados] = useState([])
     const token = localStorage.getItem('token')
     console.log("Token Home", token)
 
-    // Explicar novamente no dia 07/02/25
-    useEffect(()=>{
-        const fetchData = async ()=>{
+    useEffect(() => {
+
+        if (!token) return;
+
+        const fetchData = async () => {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/api/professores',
                     {
@@ -17,15 +21,66 @@ export default function Home(){
                         }
                     }
                 )
+                setDados(response.data)
             } catch (error) {
-                
+                console.log(error)
             }
         }
+
+        fetchData()
     }, [])
 
-    return(
-        <>
-        <h1>Página Home</h1>
-        </>
+    return (
+        <div className="container_home">
+            <body>
+                <header>
+                    <h1>Lista de professores</h1>
+                </header>
+
+                <div className="lista">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Ações</th>
+                                <th>ID</th>
+                                <th>NI</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Telefone</th>
+                                <th>Ocupação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dados.map((professor) => (
+                                <tr key={professor.id}>
+                                    <td>
+                                        <FaEdit className="edit" />
+                                        <FaTrash className="delete" />
+                                    </td>
+                                    <td>{professor.id}</td>
+                                    <td>{professor.ni}</td>
+                                    <td>{professor.nome}</td>
+                                    <td>{professor.email}</td>
+                                    <td>{professor.tel}</td>
+                                    <td>{professor.ocupacao}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                
+                <footer>
+                    <div className="btn1">
+                        <FaPlus className="adicionar"/>
+                    </div>
+                    <div className="pesquisar">
+                        <input placeholder="id"/>
+                    </div>
+                    <div className="btn2">
+                        <FaSearch className="procurar" />
+                    </div>
+                </footer>
+            </body>
+        </div>
     )
 }
